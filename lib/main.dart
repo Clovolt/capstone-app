d77bc9eccf0f341351fb04f8896d6cb4564abef0
Mehdi Tuzla <mehdi@meeapps.com.tr>
2022-06-07 22:25:02 +0300

    Updated version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:ffi';
+import 'dart:math';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'dart:io';
@@ -6,12 +6,6 @@ import 'dart:async';
 import 'package:audio_session/audio_session.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:just_waveform/just_waveform.dart';
-import 'dart:convert';
-import 'package:flutter/services.dart';
-import 'package:rxdart/rxdart.dart';
-import 'package:capstone_app/cantinaBandTest.dart';
-
-
 
 void main() async {
   runApp(const MyApp());
@@ -36,7 +30,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title,}) : super(key: key);
-
   final String title;
 
   @override
@@ -49,20 +42,27 @@ class _MyHomePageState extends State<MyHomePage> {
   bool init = false;
   bool isRecording = false;
   bool isListening = false;
+  bool isThereNewRecord = false;
   bool connectionError = false;
   List<int> savedData = [];
+  List<int> waveformData = [];
   String state = "Not Recording";
+  String filePath = '/storage/emulated/0/Capstone/recordedFile.wav';
+  String waveformPath = '/storage/emulated/0/Capstone/waveform1.wave';
   late Socket socket;
 
   late Duration? fileDuration;
   late Duration? currentDuration;
+
   double sliderValue = 0.0;
+
+  late Stream<WaveformProgress> progressStream;
 
   void startRecording() async {
 
     try {
       socket = await Socket
-          .connect('3.121.188.154', 1337);
+          .connect('35.198.127.205', 1337);
     } catch (e) {
       setState(() {
         connectionError = true;
@@ -83,22 +83,21 @@ class _MyHomePageState extends State<MyHomePage> {
       });
 
       socket.listen((List<int> dataStream) {
-        print(dataStream);
         savedData.addAll(dataStream);
       });
 
 
-      await Future.delayed(const Duration(seconds: 5));
+      await Future.delayed(const Duration(seconds: 10));
       await socket.close();
       print('Disconnected!');
       print("Data length: ${savedData.length}");
-      //print(savedData);
+
       await save(savedData, 44100);
       savedData.clear();
 
       // Try to load audio from a source and catch any errors.
       try {
-        await player.setFilePath("/storage/emulated/0/Capstone/recordedFile.wav");
+        await player.setFilePath(filePath);
         print('Just Audio initialized!');
       } catch (e) {
         print("Error loading audio source: $e");
@@ -112,7 +111,24 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> save(List<int> data, int sampleRate) async {
-    File recordedFile = File("/storage/emulated/0/Capstone/recordedFile.wav");
+    setState(() {
+      isThereNewRecord = true;
+    });
+
+    File recordedFile = File(filePath);
+
+    List<int> data1 = [];
+
+    for (int i = 0; i < data.length - 1; i+=2) {
+
+      int a = ((data[i + 1] << 8) | (data[i]));
+
+
+      data1.add(a & 0x00FF);
+      data1.add((a & 0xFF00) >> 8);
+
+      if (i >= data.length - 2) break;
+    }
 
     var channels = 1;
 
@@ -159,127 +175,17 @@ class _MyHomePageState extends State<MyHomePage> {
       (size >> 8) & 0xff,
       (size >> 16) & 0xff,
       (size >> 24) & 0xff,
-      ...data
+      ...data1
     ]);
     recordedFile.writeAsBytesSync(header, flush: true);
 
-    /////////
-    File recordedFile2 = File("/storage/emulated/0/Capstone/recordedFile2.wav");
-
-    List<int> data2 = [];
-
-    for (int i = 0; i < data.length; i++) {
-      data2.add(data[i] * 2);
-    }
-
-    var size2 = data2.length;
-
-    var fileSize2 = size2 + 36;
-
-    Uint16List header2 = Uint16List.fromList([
-      // "RIFF"
-      82, 73, 70, 70,
-      fileSize2 & 0xff,
-      (fileSize2 >> 8) & 0xff,
-      (fileSize2 >> 16) & 0xff,
-      (fileSize2 >> 24) & 0xff,
-      // WAVE
-      87, 65, 86, 69,
-      // fmt
-      102, 109, 116, 32,
-      // fmt chunk size 16
-      16, 0, 0, 0,
-      // Type of format
-      1, 0,
-      // One channel
-      channels, 0,
-      // Sample rate
-      sampleRate & 0xff,
-      (sampleRate >> 8) & 0xff,
-      (sampleRate >> 16) & 0xff,
-      (sampleRate >> 24) & 0xff,
-      // Byte rate
-      byteRate & 0xff,
-      (byteRate >> 8) & 0xff,
-      (byteRate >> 16) & 0xff,
-      (byteRate >> 24) & 0xff,
-      // Uhm
-      ((16 * channels) / 8).round(), 0,
-      // bitsize
-      16, 0,
-      // "data"
-      100, 97, 116, 97,
-      size2 & 0xff,
-      (size2 >> 8) & 0xff,
-      (size2 >> 16) & 0xff,
-      (size2 >> 24) & 0xff,
-      ...data2
-    ]);
-    recordedFile2.writeAsBytesSync(header2, flush: true);
-
-    /////////
-    File recordedFile3 = File("/storage/emulated/0/Capstone/recordedFile3.wav");
-
-    List<int> data3 = [];
-
-    for (int i = 0; i < data.length; i+=2) {
-      int a = ((data[i] << 8) | data[i+1]);
-      data3.add(a);
-    }
-
-    var size3 = data3.length;
-
-    var fileSize3 = size3 + 36;
-
-    sampleRate = (sampleRate / 2).round();
-
-
-
-    Uint16List header3 = Uint16List.fromList([
-      // "RIFF"
-      82, 73, 70, 70,
-      fileSize3 & 0xff,
-      (fileSize3 >> 8) & 0xff,
-      (fileSize3 >> 16) & 0xff,
-      (fileSize3 >> 24) & 0xff,
-      // WAVE
-      87, 65, 86, 69,
-      // fmt
-      102, 109, 116, 32,
-      // fmt chunk size 16
-      16, 0, 0, 0,
-      // Type of format
-      1, 0,
-      // One channel
-      channels, 0,
-      // Sample rate
-      sampleRate & 0xff,
-      (sampleRate >> 8) & 0xff,
-      (sampleRate >> 16) & 0xff,
-      (sampleRate >> 24) & 0xff,
-      // Byte rate
-      byteRate & 0xff,
-      (byteRate >> 8) & 0xff,
-      (byteRate >> 16) & 0xff,
-      (byteRate >> 24) & 0xff,
-      // Uhm
-      ((16 * channels) / 8).round(), 0,
-      // bitsize
-      16, 0,
-      // "data"
-      100, 97, 116, 97,
-      size3 & 0xff,
-      (size3 >> 8) & 0xff,
-      (size3 >> 16) & 0xff,
-      (size3 >> 24) & 0xff,
-      ...data3
-    ]);
-    recordedFile3.writeAsBytesSync(header3, flush: true);
-
-    print(data[data.length-2]);
-    print(data[data.length-1]);
-    print(data3[data3.length-1]);
-
+    progressStream.listen((waveformProgress) {
+      if ((100 * waveformProgress.progress).toInt() == 100) print('Progress: %${(100 * waveformProgress.progress).toInt()}');
+      if (waveformProgress.waveform != null) {
+        waveformData.clear();
+        waveformData = waveformProgress.waveform!.data;
+      }
+    });
   }
 
   _initPlayer() async {
@@ -300,9 +206,6 @@ class _MyHomePageState extends State<MyHomePage> {
     player.positionStream.listen((event) {
       currentDuration = event;
       setState(() {
-        //print(Duration(milliseconds: (fileDuration!.inMilliseconds * (currentDuration!.inMilliseconds / fileDuration!.inMilliseconds)).toInt()).toString());
-        print(player.position);
-
         if (currentDuration == player.duration) {
           player.seek(Duration.zero);
           player.stop();
@@ -313,11 +216,9 @@ class _MyHomePageState extends State<MyHomePage> {
       });
     },);
 
-    player.setVolume(1);
-
     // Try to load audio from a source and catch any errors.
     try {
-      await player.setFilePath("/storage/emulated/0/Capstone/recordedFile.wav");
+      await player.setFilePath(filePath);
       print('Just Audio initialized!');
     } catch (e) {
       print("Error loading audio source: $e");
@@ -329,11 +230,9 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   checkFile () async {
-    File newFile = File("/storage/emulated/0/Capstone/recordedFile.wav");
+    File newFile = File(filePath);
 
     bool isFileExist = await newFile.exists();
-
-    print(isFileExist);
 
     if(!isFileExist) {
 
@@ -394,6 +293,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // TODO: implement initState
     super.initState();
     checkFile();
+
     _initPlayer();
   }
 
@@ -406,8 +306,15 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    progressStream = JustWaveform.extract(
+      audioInFile: File(filePath),
+      waveOutFile: File(waveformPath),
+      zoom: const WaveformZoom.pixelsPerSecond(100),
+    );
+
     return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
       appBar: AppBar(
         title: Text(widget.title),
       ),
@@ -416,6 +323,37 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Container(
+              height: 200,
+              padding: const EdgeInsets.only(bottom: 50),
+              width: double.maxFinite,
+              child: StreamBuilder<WaveformProgress>(
+                stream: progressStream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text(
+                        'Error: ${snapshot.error}',
+                        style: Theme.of(context).textTheme.headline6,
+                        textAlign: TextAlign.center,
+                      ),
+                    );
+                  }
+                  final waveform = snapshot.data?.waveform;
+                  if (waveform == null) {
+                    return const Center();
+                  }
+                  return AudioWaveformWidget(
+                    waveform: waveform,
+                    start: Duration.zero,
+                    duration: waveform.duration,
+                    strokeWidth: 1.5,
+                    pixelsPerStep: 2.5,
+                    scale: 75,
+                  );
+                },
+              ),
+            ),
             Slider(
               value: fileDuration!.inMilliseconds == 0 ? 0 : currentDuration!.inMilliseconds / fileDuration!.inMilliseconds,
               onChanged: (value) {
@@ -449,7 +387,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     const Spacer(),
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: Text(player.duration!.toString().split('.')[0]),
+                      child: Text(player.duration! < const Duration(seconds: 1) ? "" : player.duration!.toString().split('.')[0]),
                     ),
                   ],
                 ),),
@@ -485,5 +423,114 @@ class _MyHomePageState extends State<MyHomePage> {
             : const Text('Start recording'),
       ),
     );
+  }
+}
+
+class AudioWaveformWidget extends StatefulWidget {
+  final Color waveColor;
+  final double scale;
+  final double strokeWidth;
+  final double pixelsPerStep;
+  final Waveform waveform;
+  final Duration start;
+  final Duration duration;
+
+  const AudioWaveformWidget({
+    Key? key,
+    required this.waveform,
+    required this.start,
+    required this.duration,
+    this.waveColor = Colors.blue,
+    this.scale = 1.0,
+    this.strokeWidth = 5.0,
+    this.pixelsPerStep = 8.0,
+  }) : super(key: key);
+
+  @override
+  _AudioWaveformState createState() => _AudioWaveformState();
+}
+
+class _AudioWaveformState extends State<AudioWaveformWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRect(
+      child: CustomPaint(
+        painter: AudioWaveformPainter(
+          waveColor: widget.waveColor,
+          waveform: widget.waveform,
+          start: widget.start,
+          duration: widget.duration,
+          scale: widget.scale,
+          strokeWidth: widget.strokeWidth,
+          pixelsPerStep: widget.pixelsPerStep,
+        ),
+      ),
+    );
+  }
+}
+
+class AudioWaveformPainter extends CustomPainter {
+  final double scale;
+  final double strokeWidth;
+  final double pixelsPerStep;
+  final Paint wavePaint;
+  final Waveform waveform;
+  final Duration start;
+  final Duration duration;
+
+  AudioWaveformPainter({
+    required this.waveform,
+    required this.start,
+    required this.duration,
+    Color waveColor = Colors.blue,
+    this.scale = 1.0,
+    this.strokeWidth = 5.0,
+    this.pixelsPerStep = 8.0,
+  }) : wavePaint = Paint()
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = strokeWidth
+    ..strokeCap = StrokeCap.round
+    ..color = waveColor;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (duration == Duration.zero) return;
+
+    double width = size.width;
+    double height = size.height;
+
+    final waveformPixelsPerWindow = waveform.positionToPixel(duration).toInt();
+    final waveformPixelsPerDevicePixel = waveformPixelsPerWindow / width;
+    final waveformPixelsPerStep = waveformPixelsPerDevicePixel * pixelsPerStep;
+    final sampleOffset = waveform.positionToPixel(start);
+    final sampleStart = -sampleOffset % waveformPixelsPerStep;
+    for (var i = sampleStart.toDouble();
+    i <= waveformPixelsPerWindow + 1.0;
+    i += waveformPixelsPerStep) {
+      final sampleIdx = (sampleOffset + i).toInt();
+      final x = i / waveformPixelsPerDevicePixel;
+      final minY = normalise(waveform.getPixelMin(sampleIdx), height);
+      final maxY = normalise(waveform.getPixelMax(sampleIdx), height);
+      canvas.drawLine(
+        Offset(x + strokeWidth / 2, max(strokeWidth * 0.75, minY)),
+        Offset(x + strokeWidth / 2, min(height - strokeWidth * 0.75, maxY)),
+        wavePaint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant AudioWaveformPainter oldDelegate) {
+    return false;
+  }
+
+  double normalise(int s, double height) {
+    if (waveform.flags == 0) {
+      final y = 32768 + (scale * s).clamp(-32768.0, 32767.0).toDouble();
+      return height - 1 - y * height / 65536;
+    } else {
+      final y = 128 + (scale * s).clamp(-128.0, 127.0).toDouble();
+      return height - 1 - y * height / 256;
+    }
   }
 }
